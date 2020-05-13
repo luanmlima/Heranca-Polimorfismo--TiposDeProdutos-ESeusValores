fb964e240d543c42945777ff9730416708ea197c
Luan Martins <lianm.lima366@gmail.com>
2020-05-13 11:14:32 -0300

    Tipos de produtos e seus respectivos preços

---
+++ Course/Entities/UserProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Course.Entities
+{
+    class UserProduct : Product
+    {
+        public DateTime Date { get; set; }
+
+        public UserProduct()
+        {
+        }
+
+        public UserProduct(string name, double price, DateTime date)
+            : base(name, price)
+        {
+            Date = date;
+        }
+
+        public override string PriceTag()
+        {
+            return Name 
+                + " (used) $ " 
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: " 
+                + Date.ToString("dd/MM/yyyy")
+                +")";
+        }
+        
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char tag = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (tag == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (tag == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+
+                else
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UserProduct(name, price, date)); 
+                }
+                Console.WriteLine();
+                
+            }
+
+            Console.WriteLine("PRICE TAGS:");
+            Console.WriteLine();
+            foreach(Product tags in list)
+            {
+                Console.WriteLine(tags.PriceTag().ToString());
+                
+            }
+        }
+    }
+}
